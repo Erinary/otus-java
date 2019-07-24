1c806f566c320f1e3559c18644357627dff8c45b
Liubov Udalova <udalova.lbv@gmail.com>
2019-07-24 13:18:15 +0300

    hw-08-serializer - тест для сериализатора

--- hw08-serializer/src/main/java/ru/otus/erinary/entity/Person.java
+++ hw08-serializer/src/main/java/ru/otus/erinary/entity/Person.java
@@ -1,10 +1,12 @@
 package ru.otus.erinary.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.util.List;
 
 @Data
+@AllArgsConstructor
 public class Person {
 
     private String name;
---
+++ hw08-serializer/src/test/java/ru/otus/erinary/serializer/MySerializerTest.java
@@ -0,0 +1,31 @@
+package ru.otus.erinary.serializer;
+
+import com.google.gson.Gson;
+import org.junit.jupiter.api.Test;
+import ru.otus.erinary.entity.Person;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MySerializerTest {
+
+    @Test
+    void testToJson() {
+        Gson gson = new Gson();
+        MySerializer serializer = new MySerializer();
+
+        String[] hobbies = {"travel", "board games"};
+        Person person = new Person(
+                "Jack",
+                25,
+                "London",
+                List.of("Cat Martin", "Parrot Duke"),
+                hobbies
+        );
+
+        String json = serializer.toJson(person);
+        System.out.println(json);
+        assertEquals(person, gson.fromJson(json, Person.class));
+    }
+}
