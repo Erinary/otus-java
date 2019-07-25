ea55512f6c8663b40d8dc2d4b8c599f5f927bbdf
Liubov Udalova <udalova.lbv@gmail.com>
2019-07-25 15:39:39 +0300

    hw-08-serializer - исправление - работа со разными объектами

--- hw08-serializer/src/main/java/ru/otus/erinary/serializer/MySerializer.java
+++ hw08-serializer/src/main/java/ru/otus/erinary/serializer/MySerializer.java
@@ -1,6 +1,6 @@
 package ru.otus.erinary.serializer;
 
-import ru.otus.erinary.entity.Person;
+import org.apache.commons.lang3.ClassUtils;
 
 import javax.json.*;
 import java.io.StringWriter;
@@ -14,14 +14,58 @@ class MySerializer {
     private final static String STATIC = "static";
     private final static String TRANSIENT = "transient";
 
-    String toJson(Person person) {
-        JsonObject jsonObject = prepareJsonObject(person);
-
+    String toJson(Object object) {
+        JsonValue jsonObject = prepareJsonValue(object);
         StringWriter strWriter = new StringWriter();
         try (JsonWriter jsonWriter = Json.createWriter(strWriter)) {
-            jsonWriter.writeObject(jsonObject);
+            jsonWriter.write(jsonObject);
         }
         return strWriter.getBuffer().toString();
+    }
+
+    private JsonValue prepareJsonValue(Object object) {
+        JsonArrayBuilder arrayBuilder = Json.createArrayBuilder();
+        if (object == null) {
+            return JsonValue.NULL;
+        } else if (ClassUtils.isPrimitiveOrWrapper(object.getClass()) || object instanceof String) {
+            return handlePrimitivesAndString(object);
+        } else if (Collection.class.isAssignableFrom(object.getClass())) {
+            for (Object element : (Collection) object) {
+                arrayBuilder.add(handlePrimitivesAndString(element));
+            }
+            return arrayBuilder.build();
+        } else if (object.getClass().isArray()) {
+            for (int i = 0; i < Array.getLength(object); ++i) {
+                arrayBuilder.add(handlePrimitivesAndString(Array.get(object, i)));
+            }
+            return arrayBuilder.build();
+        } else {
+            return prepareJsonObject(object);
+        }
+    }
+
+    private JsonValue handlePrimitivesAndString(Object object) {
+        if (object instanceof Integer) {
+            return Json.createValue((int) object);
+        } else if (object instanceof Short) {
+            return Json.createValue((short) object);
+        } else if (object instanceof Byte) {
+            return Json.createValue((byte) object);
+        } else if (object instanceof Long) {
+            return Json.createValue((long) object);
+        } else if (object instanceof Double) {
+            return Json.createValue((double) object);
+        } else if (object instanceof Float) {
+            return Json.createValue((float) object);
+        } else if (object instanceof Boolean) {
+            if ((Boolean) object) {
+                return JsonValue.TRUE;
+            } else {
+                return JsonValue.FALSE;
+            }
+        } else {
+            return Json.createValue(object.toString());
+        }
     }
 
     private JsonObject prepareJsonObject(Object object) {
--- hw08-serializer/src/test/java/ru/otus/erinary/serializer/MySerializerTest.java
+++ hw08-serializer/src/test/java/ru/otus/erinary/serializer/MySerializerTest.java
@@ -4,6 +4,7 @@ import com.google.gson.Gson;
 import org.junit.jupiter.api.Test;
 import ru.otus.erinary.entity.Person;
 
+import java.util.Collections;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -27,6 +28,26 @@ class MySerializerTest {
 
         String json = serializer.toJson(person);
         System.out.println(json);
-        assertEquals(person, gson.fromJson(json, Person.class));
+        assertEquals(gson.fromJson(json, Person.class), person);
+    }
+
+    @Test
+    void testDifferentTypesToJson() {
+        Gson gson = new Gson();
+        MySerializer serializer = new MySerializer();
+
+        assertEquals(gson.toJson(null), serializer.toJson(null));
+        assertEquals(gson.toJson((byte) 1), serializer.toJson((byte) 1));
+        assertEquals(gson.toJson((short) 1), serializer.toJson((short) 1));
+        assertEquals(gson.toJson(1), serializer.toJson(1));
+        assertEquals(gson.toJson(1L), serializer.toJson(1L));
+        assertEquals(gson.toJson(1f), serializer.toJson(1f));
+        assertEquals(gson.toJson(1d), serializer.toJson(1d));
+        assertEquals(gson.toJson("aaa"), serializer.toJson("aaa"));
+        assertEquals(gson.toJson('a'), serializer.toJson('a'));
+        assertEquals(gson.toJson(true), serializer.toJson(true));
+        assertEquals(gson.toJson(new int[]{1, 2, 3}), serializer.toJson(new int[]{1, 2, 3}));
+        assertEquals(gson.toJson(List.of(1, 2, 3)), serializer.toJson(List.of(1, 2, 3)));
+        assertEquals(gson.toJson(Collections.singletonList(1)), serializer.toJson(Collections.singletonList(1)));
     }
 }
