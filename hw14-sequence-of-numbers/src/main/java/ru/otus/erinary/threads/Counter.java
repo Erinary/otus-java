6776aaf40467dd1c4456f6f25c9873343e22926e
Liubov Udalova <udalova.lbv@gmail.com>
2019-08-08 18:31:37 +0300

    hw-14-sequence-of-numbers - вариант с декрементом

--- hw14-sequence-of-numbers/src/main/java/ru/otus/erinary/threads/Counter.java
+++ hw14-sequence-of-numbers/src/main/java/ru/otus/erinary/threads/Counter.java
@@ -1,17 +1,32 @@
 package ru.otus.erinary.threads;
 
+@SuppressWarnings("WeakerAccess")
 public class Counter {
     private int count;
+    private int step;
+    private int target;
 
     public Counter(int count) {
         this.count = count;
+        this.step = 1;
+        this.target = 10;
     }
 
-    public void add(int num) {
-        count += num;
+    public void tick() {
+        count += step;
     }
 
     public int getCount() {
         return count;
+    }
+
+    public void enforceBorderConditions() {
+        if (count == 10) {
+            step = -1;
+            target = 1;
+        } else if (count == 1) {
+            step = 1;
+            target = 10;
+        }
     }
 }
--- hw14-sequence-of-numbers/src/main/java/ru/otus/erinary/threads/MyThread.java
+++ hw14-sequence-of-numbers/src/main/java/ru/otus/erinary/threads/MyThread.java
@@ -7,6 +7,7 @@ public class MyThread extends Thread {
     private int lastRead;
     private final Object monitor;
 
+    @SuppressWarnings("WeakerAccess")
     public MyThread(Counter counter, Object monitor, int threadNumber) {
         this.counter = counter;
         this.lastSet = counter.getCount() - 1;
@@ -17,23 +18,29 @@ public class MyThread extends Thread {
 
     @Override
     public void run() {
-        int num = 1;
         while (true) {
+            sleep();
             synchronized (monitor) {
                 if (lastRead != counter.getCount()) {
                     lastRead = counter.getCount();
                     System.out.println(currentThread().getName() + ": GET " + counter.getCount());
                 }
-                if (counter.getCount() == 10) {
-                    break;
-                }
                 if (lastSet != counter.getCount()) {
-                    counter.add(num);
+                    counter.tick();
+                    counter.enforceBorderConditions();
                     lastSet = counter.getCount();
                     lastRead = counter.getCount();
                     System.out.println(currentThread().getName() + ": SET " + counter.getCount());
                 }
             }
+        }
+    }
+
+    private void sleep() {
+        try {
+            sleep(500);
+        } catch (InterruptedException e) {
+            currentThread().interrupt();
         }
     }
 
