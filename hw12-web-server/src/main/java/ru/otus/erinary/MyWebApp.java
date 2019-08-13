ed12ab0e3bb81bb76562e57bc7af7047bff19f69
Erinary <udalova.lbv@gmail.com>
2019-08-14 01:45:31 +0300

    hw12-web-server - починили статику

--- hw12-web-server/src/main/java/ru/otus/erinary/MyWebApp.java
+++ hw12-web-server/src/main/java/ru/otus/erinary/MyWebApp.java
@@ -1,17 +1,15 @@
 package ru.otus.erinary;
 
-import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.server.handler.HandlerList;
-import org.eclipse.jetty.server.handler.ResourceHandler;
+import org.eclipse.jetty.servlet.DefaultServlet;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
+import org.eclipse.jetty.util.resource.Resource;
 import ru.otus.erinary.dbutils.DataBaseUtils;
 import ru.otus.erinary.model.User;
 import ru.otus.erinary.orm.DBServiceImpl;
 import ru.otus.erinary.servlet.UserControllerServlet;
 
-import java.net.URL;
 import java.sql.SQLException;
 
 public class MyWebApp {
@@ -46,30 +44,20 @@ public class MyWebApp {
     private Server createServer() {
         Server server = new Server(PORT);
         ServletContextHandler context = new ServletContextHandler();
-        context.addServlet(new ServletHolder(createUserControllerServlet()), "/home/users");
-        server.setHandler(context);
 
-//        HandlerList handlerList = new HandlerList();
-//        handlerList.setHandlers(new Handler[]{context, createResourceHandler()});
-//        server.setHandler(handlerList);
+        context.setBaseResource(
+                Resource.newResource(MyWebApp.class.getClassLoader().getResource("web"))
+        );
+        context.addServlet(new ServletHolder("default", DefaultServlet.class), "/");
+        context.addServlet(new ServletHolder(createUserControllerServlet()), "/users");
+
+        server.setHandler(context);
 
         return server;
     }
 
     private UserControllerServlet createUserControllerServlet() {
         return new UserControllerServlet(new DBServiceImpl<>(dataBaseUtils.getSessionFactory(), User.class));
-    }
-
-    private ResourceHandler createResourceHandler() {
-        ResourceHandler resourceHandler = new ResourceHandler();
-        resourceHandler.setDirectoriesListed(false);
-        resourceHandler.setWelcomeFiles(new String[]{"index.html"});
-        URL fileDir = MyWebApp.class.getClassLoader().getResource("web");
-        if (fileDir == null) {
-            throw new RuntimeException("File Directory not found");
-        }
-        resourceHandler.setResourceBase(fileDir.getPath());
-        return resourceHandler;
     }
 
     private void shutdown() throws SQLException {
