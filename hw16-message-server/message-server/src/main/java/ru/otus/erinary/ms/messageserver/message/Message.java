ab4675f7a571ce26c2857c2e0e61666bd639dc6d
Liubov Udalova <udalova.lbv@gmail.com>
2019-09-08 16:54:31 +0300

    hw-16-message-server - убран jackson из message-server

--- hw16-message-server/message-server/src/main/java/ru/otus/erinary/ms/messageserver/message/Message.java
+++ hw16-message-server/message-server/src/main/java/ru/otus/erinary/ms/messageserver/message/Message.java
@@ -1,27 +1,9 @@
 package ru.otus.erinary.ms.messageserver.message;
 
-import com.fasterxml.jackson.annotation.JsonSubTypes;
-import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import lombok.Data;
-import ru.otus.erinary.ms.messageserver.message.create.CreateUserRequest;
-import ru.otus.erinary.ms.messageserver.message.create.CreateUserResponse;
-import ru.otus.erinary.ms.messageserver.message.load.LoadUsersRequest;
-import ru.otus.erinary.ms.messageserver.message.load.LoadUsersResponse;
-import ru.otus.erinary.ms.messageserver.message.util.ErrorMessage;
 
 import java.io.Serializable;
 
-@JsonTypeInfo(
-        use = JsonTypeInfo.Id.NAME,
-        property = "type"
-)
-@JsonSubTypes({
-        @JsonSubTypes.Type(value = CreateUserRequest.class, name = "createUserRequest"),
-        @JsonSubTypes.Type(value = CreateUserResponse.class, name = "createUserResponse"),
-        @JsonSubTypes.Type(value = LoadUsersRequest.class, name = "loadUsersRequest"),
-        @JsonSubTypes.Type(value = LoadUsersResponse.class, name = "loadUsersResponse"),
-        @JsonSubTypes.Type(value = ErrorMessage.class, name = "errorResponse")
-})
 @Data
 public abstract class Message implements Serializable {
 
